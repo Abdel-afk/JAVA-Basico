1c9872dabc57126c760edbed69ce44f4995e17bf
Abdelhakim <abdelhakim8304@gmail.com>
2023-02-28 11:17:34 +0100

    Sentencias if else

---
+++ src/com/company/IfElse.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        boolean check = 5 < 10;
+
+        int num1 = 50;
+        int num2 = 10;
+        int num3 = 20;
+        int num4 = 10000;
+
+        if (num1 < num2 && num2 < num3) {
+            System.out.println("verdadero");
+            System.out.println("verdadero");
+        } else if (num3 < num4) {
+            System.out.println("else if");
+        } else {
+
+        }
+
+        System.out.println("fin");
+    }
+}
